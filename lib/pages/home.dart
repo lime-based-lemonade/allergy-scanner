4e365c88b9837c0fea1da4351d3b1ef65ab7a228
Lana <lana100705@icloud.com>
2024-06-28 18:42:02 +0300

    AddScanButton

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lime_based_application/pages/scanner.dart';
+import 'package:lime_based_application/routes.dart';
+
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, ApplicationRoutes.scanner);
+          },
+          child: const Text('Scan'),
+        ),
+      ),
+    );
   }
 }
