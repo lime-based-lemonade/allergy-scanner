4f73688e9815e08790e74dcaece5258ad62b15da
Otkisaev <polarevia@bk.ru>
2024-06-30 18:04:36 +0300

    fixed ver

--- lib/pages/food_search.dart
+++ lib/pages/food_search.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
 
 class FoodSearchScreen extends StatefulWidget {
   @override
--- lib/routes.dart
+++ lib/routes.dart
@@ -15,13 +15,11 @@ class ApplicationRoutes {
   /// Get the map of application routes with widget builder functions
   static  Map<String, WidgetBuilder> getRoutesMap() => {
     home: (context) => const HomePage(),
-<<<<<<< wrapper
-    allergenSelector: (context) => const AllergenSelectorPage(),
+
     scanner: (context) => const ScannerPage(),
     foodSearch: (context) => FoodSearchScreen(),
-=======
+
     allergenSelector: (context) => AllergenSelectorPage(),
     scanner: (context) => const ScannerPage()
->>>>>>> master
   };
 }
