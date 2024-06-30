07d279cfdfacbe9ac55b5b78c1f4d8f742d1ea56
Otkisaev <polarevia@bk.ru>
2024-06-30 21:38:26 +0300

    tests_done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,88 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:lime_based_application/main.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+import 'package:lime_based_application/pages/home.dart';
+import 'package:lime_based_application/routes.dart';
+import 'package:lime_based_application/pages/scanner.dart';
+import 'package:lime_based_application/pages/allergen_selector.dart';
+import 'package:lime_based_application/widgets/allergen_list.dart';
+import 'package:lime_based_application/widgets/history_table.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  setUpAll(() {
+    HttpOverrides.global = null;
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  testWidgets('HomePage displays elements and responds to interactions', (WidgetTester tester) async {
+    await mockNetworkImagesFor(() async {
+      // Arrange
+      await tester.pumpWidget(
+        ProviderScope(
+          child: MaterialApp(
+            home: HomePage(),
+            routes: {
+              ApplicationRoutes.scanner: (context) => ScannerPage(),
+              ApplicationRoutes.allergenSelector: (context) => AllergenSelectorPage(),
+            },
+          ),
+        ),
+      );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      expect(find.text('Allergen Scanner'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      expect(find.text('My allergens'), findsOneWidget);
+
+      expect(find.text('History'), findsOneWidget);
+
+      expect(find.text('Scan'), findsOneWidget);
+
+      expect(find.byType(FloatingActionButton), findsOneWidget);
+
+      await tester.tap(find.text('Scan'));
+      await tester.pumpAndSettle();
+      expect(find.byType(ScannerPage), findsOneWidget);
+
+      Navigator.pop(tester.element(find.byType(ScannerPage)));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+      expect(find.byType(AllergenSelectorPage), findsOneWidget);
+
+      Navigator.pop(tester.element(find.byType(AllergenSelectorPage)));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byIcon(Icons.brightness_6));
+      await tester.pump();
+
+    });
+  });
+
+  testWidgets('AllergenSelectorPage displays elements and responds to interactions', (WidgetTester tester) async {
+    await mockNetworkImagesFor(() async {
+      // Arrange
+      await tester.pumpWidget(
+        ProviderScope(
+          child: MaterialApp(
+            home: AllergenSelectorPage(),
+          ),
+        ),
+      );
+
+      expect(find.text('Select allergens'), findsOneWidget);
+
+ 
+      expect(find.byType(TextField), findsOneWidget);
+
+      expect(find.byIcon(Icons.search), findsOneWidget);
+
+      await tester.enterText(find.byType(TextField), 'Milk');
+      await tester.tap(find.byIcon(Icons.search));
+      await tester.pump();
+
+      expect(find.text('Milk'), findsOneWidget);
+    });
   });
 }
