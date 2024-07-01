47d2cb64c4332368e1cbfa4724d9c89627ea0648
Dmitrii-Bargan <d.bargan@innopolis.university>
2024-07-01 23:17:09 +0300

    fature(scanner): Implement the basic scanner with placeholders

--- lib/pages/food_search.dart
+++ lib/pages/food_search.dart
@@ -3,11 +3,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lime_based_application/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+enum _DataFetchingState {
+  Idle,
+  Fetching,
+  Done
+}
 
 class FoodSearchScreen extends StatefulWidget {
-  final Set<String> allergens;
+  final List<String> allergens = ['milk', 'soy', 'bean'];
 
-  const FoodSearchScreen({super.key, required this.allergens});
+  FoodSearchScreen({super.key});
 
   @override
   _FoodSearchScreenState createState() => _FoodSearchScreenState();
@@ -20,10 +27,14 @@ class _FoodSearchScreenState extends State<FoodSearchScreen> {
   List<String> _ingredients = [];
   String _error = '';
   String _warning = '';
+  Barcode? _barcode;
+  _DataFetchingState _fetchingState = _DataFetchingState.Idle;
 
   Future<void> _fetchFoodData(String foodName) async {
+    print('Fetching data about $foodName');
+
     final url = Uri.https('api.edamam.com', '/api/food-database/v2/parser', {
-      'ingr': foodName,
+      'upc': foodName,
       'app_id': _appId,
       'app_key': _appKey,
     });
@@ -84,74 +95,155 @@ class _FoodSearchScreenState extends State<FoodSearchScreen> {
     }
   }
 
+  Widget _buildBarcode(Barcode? value) {
+    if (value == null) {
+      return const Text(
+        'Scan something!',
+        overflow: TextOverflow.fade,
+        style: TextStyle(color: Colors.white),
+      );
+    }
+
+    return Text(
+      value.displayValue ?? 'No display value.',
+      overflow: TextOverflow.fade,
+      style: const TextStyle(color: Colors.white),
+    );
+  }
+
+  void _handleBarcode(BarcodeCapture barcodes) {
+    if (mounted) {
+      setState(() {
+        _barcode = barcodes.barcodes.firstOrNull;
+        print('barcode state updated ${barcodes.barcodes.firstOrNull?.displayValue}');
+        if (_barcode?.displayValue != null) {
+          _fetchFoodData(_barcode!.displayValue!);
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(S.of(context).FindIngredients),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _foodController,
-              decoration: InputDecoration(
-                labelText: S.of(context).Enterfoodname,
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              // controller: _allergenController,
-              decoration: InputDecoration(
-                labelText: S.of(context).Enterallergenscommaseparated,
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                _fetchFoodData(_foodController.text);
-              },
-              child: Text(S.of(context).Search),
-            ),
-            const SizedBox(height: 16),
-            _error.isNotEmpty
-                ? Text(
-                    _error,
-                    style: TextStyle(color: Colors.red),
-                  )
-                : Expanded(
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: ListView.builder(
-                            itemCount: _ingredients.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text(_ingredients[index]),
-                              );
-                            },
-                          ),
-                        ),
-                        SizedBox(height: 16),
-                        Text(
-                          _warning,
-                          style: TextStyle(
-                            color: _warning.contains(S.of(context).Warning)
-                                ? Colors.red
-                                : Colors.green,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: const Text('Simple scanner')),
+      backgroundColor: Colors.black,
+      body: 
     );
   }
+
+    Widget _buildBody() {
+      switch (_fetchingState) {
+        case _DataFetchingState.Idle:
+          return _buildWidgetIdle();
+      }
+    }
+
+    Widget _buildWidgetIdle() {
+      return Stack(
+        children: [
+          MobileScanner(
+            onDetect: _handleBarcode,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              height: 100,
+              color: Colors.black.withOpacity(0.4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                      child: Center(
+                        // child: _buildBarcode(_barcode),))
+                          child: Text(_barcode == null
+                              ? 'barcode is null'
+                              : 'Error: $_error \n Warning: $_warning\nIngredients: ${_ingredients.toString()}'))),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget _buildWidgetFetching() {
+      return Placeholder();
+    }
+
+    Widget _buildWidgetDone() {
+      return Placeholder();
+    }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text(S.of(context).FindIngredients),
+  //     ),
+  //     body: Padding(
+  //       padding: const EdgeInsets.all(16.0),
+  //       child: Column(
+  //         children: [
+  //           TextField(
+  //             controller: _foodController,
+  //             decoration: InputDecoration(
+  //               labelText: S.of(context).Enterfoodname,
+  //               border: OutlineInputBorder(),
+  //             ),
+  //           ),
+  //           SizedBox(height: 16),
+  //           TextField(
+  //             // controller: _allergenController,
+  //             decoration: InputDecoration(
+  //               labelText: S.of(context).Enterallergenscommaseparated,
+  //               border: OutlineInputBorder(),
+  //             ),
+  //           ),
+  //           SizedBox(height: 16),
+  //           ElevatedButton(
+  //             onPressed: () {
+  //               _fetchFoodData(_foodController.text);
+  //             },
+  //             child: Text(S.of(context).Search),
+  //           ),
+  //           const SizedBox(height: 16),
+  //           _error.isNotEmpty
+  //               ? Text(
+  //                   _error,
+  //                   style: TextStyle(color: Colors.red),
+  //                 )
+  //               : Expanded(
+  //                   child: Column(
+  //                     children: [
+  //                       Expanded(
+  //                         child: ListView.builder(
+  //                           itemCount: _ingredients.length,
+  //                           itemBuilder: (context, index) {
+  //                             return ListTile(
+  //                               title: Text(_ingredients[index]),
+  //                             );
+  //                           },
+  //                         ),
+  //                       ),
+  //                       SizedBox(height: 16),
+  //                       Text(
+  //                         _warning,
+  //                         style: TextStyle(
+  //                           color: _warning.contains(S.of(context).Warning)
+  //                               ? Colors.red
+  //                               : Colors.green,
+  //                         ),
+  //                       ),
+  //                     ],
+  //                   ),
+  //                 ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
 
 class FoodData {
--- lib/pages/scanner.dart
+++ lib/pages/scanner.dart
@@ -1,15 +1,182 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 // QR code / bar code scanner page
 class ScannerPage extends ConsumerWidget {
-  const ScannerPage({super.key});
+  ScannerPage({super.key});
+
+  final _isContentScannedProvider = StateProvider((ref) => false);
+  final _noContentSnackbar = const SnackBar(content: Text("A snackbar it is!"));
+
+  BuildContext? _context;
+  WidgetRef? _ref;
+  ScaffoldMessengerState? _scaffoldMessengerState;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    return const BarcodeScannerSimple();
+    _context = context;
+    _ref = ref;
+    _scaffoldMessengerState = ScaffoldMessenger.of(context);
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Product scanner"),),
-      body: const Center(),
+      appBar: AppBar(
+        title: const Text("Product scanner"),
+      ),
+      body: Stack(
+        children: [
+          MobileScanner(
+            // Do not scan codes if something was already scanned
+            onDetect:
+                ref.watch(_isContentScannedProvider) ? _handleBarcode : null,
+          )
+        ],
+      ),
+    );
+  }
+
+  void _handleBarcode(BarcodeCapture barcodes) {
+    final value = barcodes.barcodes.firstOrNull?.displayValue;
+
+    if (value == null) {
+      _scaffoldMessengerState?.showSnackBar(_noContentSnackbar);
+      return;
+    }
+
+    _displayResult(value);
+  }
+
+  void _displayResult(String name) async {
+    if (_context == null) {
+      return;
+    }
+
+    // TODO: Call the API wrapper and await its response
+    bool safe = false;
+  }
+}
+
+class BarcodeScanner extends StatefulWidget {
+  const BarcodeScanner({super.key});
+
+  @override
+  State<BarcodeScanner> createState() => _BarcodeScannerState();
+}
+
+class _BarcodeScannerState extends State<BarcodeScanner> {
+  Barcode? _barcode;
+
+  Widget _buildBarcode(Barcode? value) {
+    if (value == null) {
+      return const Text(
+        'Scan something!',
+        overflow: TextOverflow.fade,
+        style: TextStyle(color: Colors.white),
+      );
+    }
+
+    return Text(
+      value.displayValue ?? 'No display value.',
+      overflow: TextOverflow.fade,
+      style: const TextStyle(color: Colors.white),
+    );
+  }
+
+  void _handleBarcode(BarcodeCapture barcodes) {
+    if (mounted) {
+      setState(() {
+        _barcode = barcodes.barcodes.firstOrNull;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        MobileScanner(
+          onDetect: _handleBarcode,
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            height: 100,
+            color: Colors.black.withOpacity(0.4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(child: Center(child: _buildBarcode(_barcode))),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BarcodeScannerSimple extends StatefulWidget {
+  const BarcodeScannerSimple({super.key});
+
+  @override
+  State<BarcodeScannerSimple> createState() => _BarcodeScannerSimpleState();
+}
+
+class _BarcodeScannerSimpleState extends State<BarcodeScannerSimple> {
+  Barcode? _barcode;
+
+  Widget _buildBarcode(Barcode? value) {
+    if (value == null) {
+      return const Text(
+        'Scan something!',
+        overflow: TextOverflow.fade,
+        style: TextStyle(color: Colors.white),
+      );
+    }
+
+    return Text(
+      value.displayValue ?? 'No display value.',
+      overflow: TextOverflow.fade,
+      style: const TextStyle(color: Colors.white),
+    );
+  }
+
+  void _handleBarcode(BarcodeCapture barcodes) {
+    if (mounted) {
+      setState(() {
+        _barcode = barcodes.barcodes.firstOrNull;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Simple scanner')),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          MobileScanner(
+            onDetect: _handleBarcode,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              height: 100,
+              color: Colors.black.withOpacity(0.4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Center(child: _buildBarcode(_barcode))),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
