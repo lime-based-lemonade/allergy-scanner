acbf95376db7d65cead090926cbf23e5c44cf833
Dmitrii-Bargan <d.bargan@innopolis.university>
2024-07-01 12:52:34 +0300

    chore(qr scanner): Add qrscan dependency

--- lib/pages/scanner.dart
+++ lib/pages/scanner.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// QR code / bar code scanner page
 class ScannerPage extends ConsumerWidget {
   const ScannerPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Product scanner"),),
+      body: const Center(),
+    );
   }
 }
